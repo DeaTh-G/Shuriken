f6cded10f7eab64b67d0d7a70398299194d16e5b
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2022-02-17 21:18:28 +0100

    [SWIF] Fix Rotation Data

--- XNCPLib/SWIF/Cast/SWCellInfo.cs
+++ XNCPLib/SWIF/Cast/SWCellInfo.cs
@@ -1,39 +1,24 @@
 ﻿using System.Numerics;
-using System.Runtime.InteropServices;
 using Amicitia.IO.Binary;
 
 namespace XNCPLib.SWIF.Cast
 {
-    [StructLayout(LayoutKind.Explicit)]
-    public struct SWCellInfoField
-    {
-        [FieldOffset(0)] public uint integer;
-        [FieldOffset(0)] public float single;
-
-        public SWCellInfoField(uint value) : this() { integer = value; }
-    }
-
     public class SWCellInfo : IBinarySerializable
     {
         public Vector3 Position { get; set; }
-        public SWCellInfoField Field0C { get; set; } = new();
-        public SWCellInfoField Field10 { get; set; } = new();
-        public ushort Rotation { get; set; }
-        public ushort Field16 { get; set; }
+        public uint Field0C { get; set; }
+        public uint Field10 { get; set; }
+        public uint Rotation { get; set; }
         public Vector3 Scale { get; set; }
 
         public void Read(BinaryObjectReader reader)
         {
             Position = new Vector3(reader.Read<float>(), reader.Read<float>(), reader.Read<float>());
-            Field0C = new SWCellInfoField(reader.Read<uint>());
-            Field10 = new SWCellInfoField(reader.Read<uint>());
-            Rotation = reader.Read<ushort>();
-            Field16 = reader.Read<ushort>();
+            Field0C = reader.Read<uint>();
+            Field10 = reader.Read<uint>();
+            Rotation = reader.Read<uint>();
 
-            if (Field16 == 1)
-                Scale = new Vector3(1.0f, 1.0f, 1.0f);
-            else
-                Scale = new Vector3(reader.Read<float>(), reader.Read<float>(), reader.Read<float>());
+            Scale = new Vector3(reader.Read<float>(), reader.Read<float>(), reader.Read<float>());
         }
 
         public void Write(BinaryObjectWriter writer) { }
